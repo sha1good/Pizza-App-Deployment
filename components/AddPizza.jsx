64837ac3005d7d954d29e967dd91ea3f6eb91a81
sha1good <bankoleyakub11@yahoo.com>
2022-12-31 11:17:29 +0100

    Adding external Url

--- components/AddPizza.jsx
+++ components/AddPizza.jsx
@@ -45,7 +45,7 @@ const AddPizza = ({ setClose }) => {
         extraOptions,
         img: url,
       };
-      await axios.post("http://localhost:3000/api/products", newProduct);
+      await axios.post("https://pizza-resturant-app.netlify.app/api/products", newProduct);
       setClose(true);
       router.push("/");
     } catch (error) {
